35d9d99bac19f9b821c4fabbc180aa0a1b6b77bc
LaffieTops <laffietopsss@gmail.com>
2025-04-21 19:43:40 +0300

    Update newfile.py

--- newfile.py
+++ newfile.py
@@ -243,13 +243,13 @@ if __name__ == "__main__":
 
 
 # Минималистичный веб-сервер для Render
-app = Flask(__name__)
+    app = Flask(__name__)
 
-@app.route('/')
-def index():
+    @app.route('/')
+    def index():
     return "Бот работает!"
 
-def run_flask():
+    def run_flask():
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
 
